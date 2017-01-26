03dfc3d23f764b0c5853e9d1243b9cf2130c5672
Jon Low <lowjonc@gmail.com>
2017-01-26 11:55:46 -0700

    react working and basic outline

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import routes from './config/routes.js'
 
-class App extends React.Component{
-  render() {
-    return (
-      <h1>Hello NVLO glerp</h1>
-    )
-  }
-}
 
-ReactDom.render(<App/>, document.getElementById('app'))
+// TOM's way of building react Components
+
+
+// class App extends React.Component{
+//   render() {
+//     return (
+//       <h1>Hello NVLO glerp</h1>
+//     )
+//   }
+// }
+
+ReactDom.render(
+  routes,
+  document.getElementById('app')
+)
